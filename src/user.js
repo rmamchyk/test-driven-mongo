9bd71161d1995318d40549c21859172d26d3ce5d
rmamchyk <roman.mamchyk@gmail.com>
2018-11-07 00:25:58 +0200

    Test for $inc update operator

--- src/user.js
+++ src/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: { type: String}
+    name: { type: String},
+    postCount: { type: Number, default: 0 }
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating a user', () => {
     let joe; 
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -40,5 +40,14 @@ describe('Updating a user', () => {
 
      it('class method findByIdAndRemove', (done) => {
         assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done);
+     });
+
+     it('increments postCount by 1', (done) => {
+        User.update({name: 'Joe'}, { $inc: {postCount: 1}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then(user => {
+                assert(user.postCount === 1);
+                done();
+            })
      });
 });
